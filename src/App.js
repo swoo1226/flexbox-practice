a24a40fb6b1a6de3e6748aa16aa1a41cb524bfaa
swoo1226 <copycat1226@gmail.com>
2019-11-30 20:14:14 +0900

    grid를 통한 3x3 분할

--- src/App.js
+++ src/App.js
@@ -7,9 +7,21 @@ function App() {
       <div className="viewer">
         <div className="innerView">
           <div className="page">
-            <div className="item1">1</div>
-            <div className="item2">2</div>
-            <div className="item3">3</div>
+            <div className="item1">
+              <div className="gridCol">1</div>
+              <div className="gridCol">2</div>
+              <div className="gridCol">3</div>
+            </div>
+            <div className="item2">
+              <div className="gridCol">4</div>
+              <div className="gridCol">5</div>
+              <div className="gridCol">6</div>
+            </div>
+            <div className="item3">
+              <div className="gridCol">7</div>
+              <div className="gridCol">8</div>
+              <div className="gridCol">9</div>
+            </div>
           </div>
         </div>
       </div>
