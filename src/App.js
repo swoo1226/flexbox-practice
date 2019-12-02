cb61f9a5a9623f16c2afe0175da9d24ae9bf5802
Peter Kim <sangwoo.kim@earlysloth.com>
2019-12-02 10:17:42 +0900

    before pull

--- src/App.js
+++ src/App.js
@@ -7,9 +7,25 @@ function App() {
       <div className="viewer">
         <div className="innerView">
           <div className="page">
-            <div className="left-content">left</div>
-            <div className="middle-content">middle</div>
-            <div className="right-content">right</div>
+            <div className="left-content">     
+              <div>
+                <div className="first">첫번째 줄</div>
+                <div className="second">두번째 줄</div>            
+                <div className="third">세번째 줄</div>
+              </div>  
+            </div>
+            <div className="middle-content">
+              <div className="first">
+                첫번째 줄
+              </div>
+            </div>
+            <div className="right-content">
+              <div>
+              <div className="first">첫번째 줄</div>
+                <div className="second">두번째 줄</div>            
+                <div className="third">세번째 줄</div>
+              </div>
+            </div>
           </div>
         </div>
       </div>
